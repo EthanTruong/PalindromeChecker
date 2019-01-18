4e1b198cab1fed98719c510243f35305cec704db
ettruong <ettruong@s.sfusd.edu>
2019-01-18 12:53:29 -0800

    finished!

--- out/source/PalindromeChecker.java
+++ out/source/PalindromeChecker.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class PalindromeChecker extends PApplet {
+
 public void setup()
 {
     String lines[] = loadStrings("palindromes.txt");
@@ -8,7 +24,6 @@ public void setup()
         else
             println(lines[i] + " is NOT a palindrome.");
 }
-
 public boolean palindrome(String word) {
     if (convert(word).equals(convert(reverse(word))))
         return true;
@@ -29,4 +44,13 @@ public String convert(String str) {
         if (Character.isLetter(str.charAt(i)))
             sNew += str.substring(i, i+1);
     return sNew.toLowerCase();
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "PalindromeChecker" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
